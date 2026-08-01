83b9bcc9279ffab324c3aff1dec1b08caae2c6b0
manoj-s2006 <manoj20it06@gmail.com>
2026-08-01 11:55:33 +0530

    rainwater

--- leet3754.java
+++ leet3754.java
@@ -1,5 +1,14 @@
 class leet3754{
     public static void main(String[] args) {
-        
+        int n=65463628;
+        //expected output:2618545120
+        String numStr = String.valueOf(n).replace("0", "");
+        int sum = 0;
+        for (int i = 0; i < numStr.length(); i++) {
+             sum += numStr.charAt(i) - '0';
+            }
+            int num = Integer.parseInt(numStr);
+            long result = (long)num * sum;
+            System.out.println(result);
     }
 }
