468ef6033986e0a86c20aa171a85f5603832169a
Manoj <manoj20it06@gmail.com>
2026-08-26 23:13:05 +0530

    Time: 0 ms (100%), Space: 43.3 MB (36.18%) - LeetHub

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        int count=0;
+
+        for (char ch:jewels.toCharArray()){
+            for (char c:stones.toCharArray()){
+                if (ch==c){
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
