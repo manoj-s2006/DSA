ed6c24b32bdc2310a4819a76c4d4aaf77273372a
Manoj <manoj20it06@gmail.com>
2026-09-06 12:26:20 +0530

    Time: 0 ms (100%), Space: 42.7 MB (61.41%) - LeetHub

---
+++ 2103-rings-and-rods/2103-rings-and-rods.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int countPoints(String rings) {
+        int[] r=new int[10];
+        int[] g=new int[10];
+        int[] b=new int[10];
+        for (int i=0;i<rings.length();i+=2){
+            int num=rings.charAt(i+1)-'0';
+            if (rings.charAt(i)=='R')r[num]++;
+            else if (rings.charAt(i)=='G')g[num]++;
+            else if (rings.charAt(i)=='B')b[num]++;
+        }
+        int count=0;
+        for(int i=0;i<10;i++){
+            if (r[i]>0 && g[i]>0 && b[i]>0){
+                count++;
+            }
+        }
+
+        return count;
+
+    }
+}
